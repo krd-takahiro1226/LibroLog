4fd4cd6b68b0192ea3a0085394d05b5953980807
Chosso <kurotak7255@gmail.com>
2024-09-08 22:58:19 +0900

    登録書籍一覧機能を別ファイルに作成し必要がなくなったため、削除

--- backend/backend/src/main/java/com/readrecords/backend/service/BackendService.java
+++
@@ -1,9 +0,0 @@
-package com.readrecords.backend.service;
-
-import java.util.List;
-
-import com.readrecords.backend.entity.ReadRecords;
-
-public interface BackendService {
-  public List<ReadRecords> findAll();
-}
