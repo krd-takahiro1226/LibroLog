66b4f9789c3b33d2593caaaff582cfaeb2126d85
Chosso <kurotak7255@gmail.com>
2024-10-15 16:37:37 +0900

    読書情報に関するServiceクラスに読書情報登録機能を追加

--- backend/backend/src/main/java/com/readrecords/backend/service/ReadRecordsService.java
+++ backend/backend/src/main/java/com/readrecords/backend/service/ReadRecordsService.java
@@ -6,4 +6,5 @@ import com.readrecords.backend.dto.UserReadRecordsDto;
 
 public interface ReadRecordsService {
   public List<UserReadRecordsDto> getReadRecordsByUserId(String user_id);
+  public String registerReadRecords(String ISBN, String user_id, String start_date, String end_date, int read_count, int priority, String memo);
 } 
