afcfbc806f92a336d0b6b23e744745c23d8623c9
Chosso <kurotak7255@gmail.com>
2024-10-17 23:23:21 +0900

    テーブル定義を変更

--- sql/create_book_records.sql
+++ sql/create_book_records.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS book_records (
     ISBN VARCHAR(13) PRIMARY KEY,
-    book_name VARCHAR(30) NOT NULL,
-    author VARCHAR(30) NOT NULL,
+    book_name VARCHAR(100) NOT NULL,
+    author VARCHAR(100) NOT NULL,
     genre VARCHAR(20),
     publication_year DATE,
     publisher VARCHAR(100)
