d8f2d953e6e423eebefaceb3a5c15661e8520875
Chosso <kurotak7255@gmail.com>
2024-11-02 01:44:12 +0900

    ログイン処理に使用するControllerを修正

--- project-root/backend/src/main/java/com/readrecords/backend/controller/UserLoginController.java
+++ project-root/backend/src/main/java/com/readrecords/backend/controller/UserLoginController.java
@@ -1,24 +1,39 @@
 package com.readrecords.backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.readrecords.backend.repository.UserLoginRepostoty;
-import com.readrecords.backend.service.UserLoginDetailsService;
+import com.readrecords.backend.entity.UserLogin;
 
-@Controller
+@RestController
 public class UserLoginController {
+
   @Autowired
-  UserLoginDetailsService userLoginDetailsService;
-  @Autowired
-  PasswordEncoder passwordEncoder;
-  @Autowired
-  UserLoginRepostoty userLoginRepostoty;
-  @RequestMapping(value = "/login", method = RequestMethod.GET)
-  public String showLoginWindow() {
-    return "userLogin";
+  private AuthenticationManager authenticationManager;
+
+  @PostMapping("/login")
+  public ResponseEntity<String> authenticateUser(@RequestBody UserLogin userlogin) {
+    try {
+      Authentication authentication = authenticationManager.authenticate(
+          new UsernamePasswordAuthenticationToken(userlogin.getUsername(), userlogin.getPassword())
+      );
+
+      SecurityContextHolder.getContext().setAuthentication(authentication);
+
+      // 認証成功のレスポンスを返却
+      return ResponseEntity.ok("Authentication successful");
+    } catch (AuthenticationException e) {
+      // 認証失敗時のエラーレスポンス
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
+    }
   }
 }
