cd1de8b9121765233c355cbd5bf52ece6303bfd7
Chosso <kurotak7255@gmail.com>
2024-11-02 00:47:32 +0900

    不要な処理を削除

--- project-root/backend/src/main/java/com/readrecords/backend/controller/UserLoginController.java
+++ project-root/backend/src/main/java/com/readrecords/backend/controller/UserLoginController.java
@@ -3,7 +3,6 @@ package com.readrecords.backend.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -21,10 +20,5 @@ public class UserLoginController {
   @RequestMapping(value = "/login", method = RequestMethod.GET)
   public String showLoginWindow() {
     return "userLogin";
-  }
-  @RequestMapping("/userRegistration")
-  @GetMapping
-  public String showRegisterWindow(){
-    return "userRegister";
   }
 }
