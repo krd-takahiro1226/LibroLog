cb64d9b8b12d8b7e45d17aaed9d06e28d0c07312
Chosso <kurotak7255@gmail.com>
2024-11-02 01:46:33 +0900

    ログイン画面とメニュー画面を追加

--- project-root/frontend/src/App.js
+++ project-root/frontend/src/App.js
@@ -1,13 +1,18 @@
 // App.js
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
+import Menu from './components/Menu';
 import './assets/styles/styles.css';
 
 function App() {
     return (
-        <div>
-            <Login />
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/menu" element={<Menu />} />
+            </Routes>
+        </Router>
     );
 }
 
