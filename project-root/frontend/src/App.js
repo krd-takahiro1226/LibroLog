f39605f7978d9e14cc7dd1f362c6839036e24e35
Chosso <kurotak7255@gmail.com>
2024-11-09 13:41:44 +0900

    書籍検索画面を追加

--- project-root/frontend/src/App.js
+++ project-root/frontend/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Menu from './components/Menu';
+import Search from './components/Search';
 import './assets/styles/styles.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
             <Routes>
                 <Route path="/login" element={<Login />} />
                 <Route path="/menu" element={<Menu />} />
+                <Route path="/searchBooks" element={<Search />} />
             </Routes>
         </Router>
     );
