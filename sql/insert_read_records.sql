fe49b07851b3125bb738856d84911df8a9c75699
Chosso <kurotak7255@gmail.com>
2024-12-15 04:09:39 +0900

    テーブル定義に従って微修正

--- sql/insert_read_records.sql
+++ sql/insert_read_records.sql
@@ -1,5 +1,5 @@
-insert read_records (record_id, ISBN, user_id, start_date, end_date, read_count, priority, memo)
-values (1, "9784575519945", "aaaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaa", "2020-01-01", "2020-01-01", 5, 1, "最高"),
-(2, "9784167110123", "1", "2020-01-01", "2020-01-01", 1, 2, "タイトルの伏線回収がアツい"),
-(3, "9784041491900", "aaaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaa", "2020-01-01", "2020-01-01", 5, 1, "テスト1"),
-(4, "9784041491901", "aaaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaa", "2020-01-01", "2020-01-01", 5, 1, "テスト2");
+insert read_records (ISBN, user_id, start_date, end_date, read_count, priority, memo)
+values ("9784575519945", "aaaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaa", "2020-01-01", "2020-01-01", 5, 1, "最高"),
+("9784167110123", "1", "2020-01-01", "2020-01-01", 1, 2, "タイトルの伏線回収がアツい"),
+("9784041491900", "aaaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaa", "2020-01-01", "2020-01-01", 5, 1, "テスト1"),
+("9784041491901", "aaaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaa", "2020-01-01", "2020-01-01", 5, 1, "テスト2");
