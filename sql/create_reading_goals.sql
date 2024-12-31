1534b9e78f9745e141eb9a6cf76d4b0725b675d3
Chosso <kurotak7255@gmail.com>
2024-12-31 17:04:07 +0900

    読書冊数目標管理テーブルと読書実績管理テーブル作成SQLを追加

---
+++ sql/create_reading_goals.sql
@@ -0,0 +1,12 @@
+CREATE TABLE reading_goals (
+    goal_id            INT AUTO_INCREMENT PRIMARY KEY,  -- PK
+    goal_read_number   INT             NOT NULL,        -- 目標冊数
+    user_id            varchar(255)    NOT NULL,        -- ユーザID(FK想定)
+    count_start_date   DATE            NOT NULL,        -- カウント開始日
+    count_end_date     DATE            NOT NULL,        -- カウント終了日
+    is_year_goal       BOOLEAN         NOT NULL,        -- 年間目標かどうか
+
+    -- 外部キー制約（例: userテーブルのuser_idを参照）
+    CONSTRAINT fk_reading_goals_user
+        FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
