0ba96c8dde418ba070185a5b02c4193457050eba
Chosso <kurotak7255@gmail.com>
2025-01-05 22:15:07 +0900

    読書目標情報テーブルに関するEntityを作成

---
+++ project-root/backend/src/main/java/com/readrecords/backend/entity/ReadingGoals.java
@@ -0,0 +1,27 @@
+package com.readrecords.backend.entity;
+
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+/** 読書目標情報 */
+@Data
+@Entity
+@Table(name = "register_book_records")
+public class ReadingGoals {
+  /** 管理ID(PK) */
+  @Id private String goalId;
+  /** 目標読書数 */
+  private Integer goalReadNumber;
+  /** ユーザID */
+  private String userId;
+  /** 目標期間開始日 */
+  private Date countStartDate;
+  /** 目標期間終了日 */
+  private Date countEndDate;
+  /** 年間目標であるかどうか */
+  private boolean isYearGoal;
+}
