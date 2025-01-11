2a36a6a96949f21395486819b672e19b2a566e70
Chosso <kurotak7255@gmail.com>
2025-01-12 02:57:09 +0900

    試験用データ投入のSQLを作成

---
+++ sql/insert_reading_records.sql
@@ -0,0 +1,6 @@
+INSERT INTO reading_records (goal_id, ISBN, user_id, read_start_date, read_end_date, read_done_date, is_read_done)
+VALUES
+(8, '9784575519945', 'aaaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaa', '2025-01-01', '2025-01-31', '2025-01-05', TRUE),
+(8, '9784041491900', 'aaaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaa', '2025-01-01', '2025-01-31', '2025-01-18', TRUE),
+(8, '9784167110123', 'aaaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaa', '2025-01-01', '2025-01-31', NULL, FALSE),
+(8, '9784041491901', 'aaaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaa', '2025-01-01', '2025-01-31', NULL, FALSE);
