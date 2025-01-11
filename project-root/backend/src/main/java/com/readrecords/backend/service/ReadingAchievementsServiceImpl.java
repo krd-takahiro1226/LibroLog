56bd3f96fd17693b28f10c0465fbbf87f393147c
Chosso <kurotak7255@gmail.com>
2025-01-12 03:05:08 +0900

    logおよび読書冊数のsetを追加

--- project-root/backend/src/main/java/com/readrecords/backend/service/ReadingAchievementsServiceImpl.java
+++ project-root/backend/src/main/java/com/readrecords/backend/service/ReadingAchievementsServiceImpl.java
@@ -5,6 +5,8 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,6 +26,8 @@ public class ReadingAchievementsServiceImpl implements ReadingAchievementsServic
 
   private final String DATE_FORMAT = "yyyyMMdd";
 
+  private static final Logger logger = LoggerFactory.getLogger(ReadingAchievementsServiceImpl.class);
+
   @Override
   public ReadingAchievementsDto getReadAchievementsByUserId(String userId) {
   // 返却用のDTOを作成
@@ -36,16 +40,21 @@ public class ReadingAchievementsServiceImpl implements ReadingAchievementsServic
   // 読書冊数目標管理テーブルからユーザのレコードを取得、フラグに応じて年間目標と月間目標を別々のDTOに格納
   // TODO 特に月間目標は複数取得されるため、SQLクエリでorder byなどする必要がありそう
   List<ReadingGoals> userReadingGoals = readingGoalsRepository.getReadingGoalsByUserId(userId, excecuteDate);
+  logger.info("読書目標: {}" , userReadingGoals);
   ReadingGoals userYearlyReadingGoals = userReadingGoals.stream().filter(s -> s.isYearGoal()).findFirst().orElse(null);
+  logger.info("年間目標: {}" , userYearlyReadingGoals);
   ReadingGoals userMonthlyReadingGoals = userReadingGoals.stream().filter(s -> !s.isYearGoal()).findFirst().orElse(null);
+  logger.info("月間目標: {}" , userMonthlyReadingGoals);
 
   // 年間目標に対する処理
+  // 年間目標の目標冊数を取得
   int yearlyGoal = userYearlyReadingGoals.getGoalReadNumber();
   // 年間目標の終了日を取得
   Date yearlyCountEndDate = userYearlyReadingGoals.getCountEndDate();
   // 年間目標冊数の取得および達成率を計算し、格納
-  int yearlyTotalBooks = readingRecordsRepository.getBooksCountByUserId(userId, yearlyCountEndDate);
-  double yearlyPercentage = yearlyTotalBooks / yearlyGoal;
+  int yearlyTotalBooks = readingRecordsRepository.getYearlyBooksCountByUserId(userId, yearlyCountEndDate);
+  double yearlyPercentage = (double)yearlyTotalBooks / yearlyGoal;
+  readingAchievementsDto.setYearlyTotalBooks(yearlyTotalBooks);
   readingAchievementsDto.setYearlyGoal(yearlyGoal);
   readingAchievementsDto.setYearlyPercentage(yearlyPercentage);
 
@@ -53,7 +62,7 @@ public class ReadingAchievementsServiceImpl implements ReadingAchievementsServic
   // 月間目標の終了日を取得
   Date monthlyCountEndDate = userMonthlyReadingGoals.getCountEndDate();
   // 月間目標冊数の取得および達成率を計算し、格納
-  int monthlyTotalBooks = readingRecordsRepository.getBooksCountByUserId(userId, monthlyCountEndDate);
+  int monthlyTotalBooks = readingRecordsRepository.getMonthlyBooksCountByUserId(userId, monthlyCountEndDate);
   readingAchievementsDto.setMonthlyRead(monthlyTotalBooks);
   readingAchievementsDto.setMonthlyGoal(userMonthlyReadingGoals.getGoalReadNumber());
 
