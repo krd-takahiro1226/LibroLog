093157e3a8c41d3f43ad85d6eed688da433be3e6
Chosso <kurotak7255@gmail.com>
2025-01-13 02:46:19 +0900

    フォーマッタを適用

--- project-root/backend/src/main/java/com/readrecords/backend/repository/BookRecordsRepository.java
+++ project-root/backend/src/main/java/com/readrecords/backend/repository/BookRecordsRepository.java
@@ -11,21 +11,21 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookRecordsRepository
-        extends CrudRepository<BookRecords, String> {
-    @Modifying
-    @Transactional
-    @Query(value = "insert into book_records "
-            + "(ISBN, book_name, author, genre, publication_year, publisher) values "
-            + "(:ISBN, :book_name, :author, :genre, :publication_year, :publisher)", nativeQuery = true)
-    void insertBookRecords(
-            @Param("ISBN") String ISBN,
-            @Param("book_name") String book_name,
-            @Param("author") String author,
-            @Param("genre") String genre,
-            @Param("publication_year") String publication_year,
-            @Param("publisher") String publisher);
+                extends CrudRepository<BookRecords, String> {
+        @Modifying
+        @Transactional
+        @Query(value = "insert into book_records "
+                        + "(ISBN, book_name, author, genre, publication_year, publisher) values "
+                        + "(:ISBN, :book_name, :author, :genre, :publication_year, :publisher)", nativeQuery = true)
+        void insertBookRecords(
+                        @Param("ISBN") String ISBN,
+                        @Param("book_name") String book_name,
+                        @Param("author") String author,
+                        @Param("genre") String genre,
+                        @Param("publication_year") String publication_year,
+                        @Param("publisher") String publisher);
 
-    @Query(value = "select * from "
-            + "book_records where ISBN = :ISBN", nativeQuery = true)
-    List<BookRecords> getBookRecordsByISBN(@Param("ISBN") String ISBN);
+        @Query(value = "select * from "
+                        + "book_records where ISBN = :ISBN", nativeQuery = true)
+        List<BookRecords> getBookRecordsByISBN(@Param("ISBN") String ISBN);
 }
--- project-root/backend/src/main/java/com/readrecords/backend/repository/ReadingRecordsRepository.java
+++ project-root/backend/src/main/java/com/readrecords/backend/repository/ReadingRecordsRepository.java
@@ -8,7 +8,8 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReadingRecordsRepository extends CrudRepository<ReadingRecords, Integer> {
+public interface ReadingRecordsRepository
+    extends CrudRepository<ReadingRecords, Integer> {
   /**
    * 指定した日付(=目標の終了日)までに読了フラグが立っているレコード数を計算し、月間で読んだ本の冊数を返却する
    *
@@ -16,16 +17,14 @@ public interface ReadingRecordsRepository extends CrudRepository<ReadingRecords,
    * @param monthlyCountEndDate
    * @return 月間で読んだ本の冊数
    */
-  @Query(
-      value =
-          "select count(1) from "
-              + "reading_records where user_id = :user_id"
-              + " and read_start_date <= :goals_count_end_date"
-              + " and read_end_date >= :goals_count_end_date"
-              + " and is_read_done = true",
-      nativeQuery = true)
+  @Query(value = "select count(1) from "
+      + "reading_records where user_id = :user_id"
+      + " and read_start_date <= :goals_count_end_date"
+      + " and read_end_date >= :goals_count_end_date"
+      + " and is_read_done = true", nativeQuery = true)
   int getMonthlyBooksCountByUserId(
-      @Param("user_id") String userId, @Param("goals_count_end_date") Date goalsCountEndDate);
+      @Param("user_id") String userId,
+      @Param("goals_count_end_date") Date goalsCountEndDate);
 
   /**
    * 指定した日付(=目標の終了日)までに読了フラグが立っているレコード数を計算し、年間で読んだ本の冊数を返却する
@@ -34,13 +33,11 @@ public interface ReadingRecordsRepository extends CrudRepository<ReadingRecords,
    * @param yearlyCountEndDate
    * @return 年間で読んだ本の冊数
    */
-  @Query(
-      value =
-          "select count(1) from "
-              + "reading_records where user_id = :user_id"
-              + " and read_end_date <= :goals_count_end_date"
-              + " and is_read_done = true",
-      nativeQuery = true)
+  @Query(value = "select count(1) from "
+      + "reading_records where user_id = :user_id"
+      + " and read_end_date <= :goals_count_end_date"
+      + " and is_read_done = true", nativeQuery = true)
   int getYearlyBooksCountByUserId(
-      @Param("user_id") String userId, @Param("goals_count_end_date") Date goalsCountEndDate);
+      @Param("user_id") String userId,
+      @Param("goals_count_end_date") Date goalsCountEndDate);
 }
