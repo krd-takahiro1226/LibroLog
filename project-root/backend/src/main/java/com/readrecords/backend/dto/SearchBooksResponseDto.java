849c2a7a77e71831d683a58a8bd7248342206ee0
Chosso <kurotak7255@gmail.com>
2025-01-13 03:03:04 +0900

    Javadocを追加

--- project-root/backend/src/main/java/com/readrecords/backend/dto/SearchBooksResponseDto.java
+++ project-root/backend/src/main/java/com/readrecords/backend/dto/SearchBooksResponseDto.java
@@ -8,6 +8,7 @@ import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 import lombok.Data;
 
+/** 書籍検索結果のレスポンス(全体情報) */
 @XmlRootElement(name = "root")
 @XmlAccessorType(XmlAccessType.FIELD)
 @Data
@@ -16,24 +17,31 @@ public class SearchBooksResponseDto {
   @XmlElement(name = "Item")
   private List<SearchBooksItemDto> items;
 
+  /** キャリア情報(PCもしくはモバイル) */
   @XmlElement(name = "carrier")
   private int carrier;
 
+  /** 検索結果総数 */
   @XmlElement(name = "count")
   private int count;
 
+  /** ページ内始追番 */
   @XmlElement(name = "first")
   private int first;
 
+  /** 検索結果ヒット数 */
   @XmlElement(name = "hits")
   private int hits;
 
+  /** ページ内終追番 */
   @XmlElement(name = "last")
   private int last;
 
+  /** ページ番号 */
   @XmlElement(name = "page")
   private int page;
 
+  /** 総ページ数 */
   @XmlElement(name = "pageCount")
   private int pageCount;
 }
