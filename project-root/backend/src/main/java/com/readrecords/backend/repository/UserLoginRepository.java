0d1ffe15aadad01fbf0c373a5d936bd214102961
sfuj11 <s.fujii.032@gmail.com>
2025-02-09 13:05:17 +0900

    ファイル命名ミスの修正

--- project-root/backend/src/main/java/com/readrecords/backend/repository/UserLoginRepository.java
+++ project-root/backend/src/main/java/com/readrecords/backend/repository/UserLoginRepository.java
@@ -1,18 +1,26 @@
 package com.readrecords.backend.repository;
 
-import com.readrecords.backend.entity.UserLogin;
 import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.readrecords.backend.entity.UserLogin;
+
 @Repository
-public interface UserLoginRepostoty extends JpaRepository<UserLogin, String> {
+public interface UserLoginRepository extends JpaRepository<UserLogin, String> {
+  // username でユーザーを検索
   @Query(value = "select * from users "
       + "where username = :username", nativeQuery = true)
   Optional<UserLogin> findByUsername(@Param("username") String username);
 
+  // user_id でユーザーを検索
+  @Query(value = "SELECT * FROM users WHERE user_id = :userId", nativeQuery = true)
+  Optional<UserLogin> findByUserId(@Param("userId") String userId);
+
+  // username でパスワードとロールを検索
   @Query(value = "select username, password, role from users "
       + "where username = :username", nativeQuery = true)
   String findPasswordByUsername(@Param("username") String username);
