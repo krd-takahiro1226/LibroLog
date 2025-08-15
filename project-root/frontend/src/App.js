fe55da5ed1524609e961de1195998ffb0f3b9b13
Chosso <kurotak7255@gmail.com>
2025-08-16 02:25:09 +0900

    新規ファイルを追加

--- project-root/frontend/src/App.js
+++ project-root/frontend/src/App.js
@@ -2,22 +2,29 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Top from './components/Top';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import './assets/styles/styles.css';
+import AchievementsSettings from './components/AchievementsSettings';
 import Login from './components/Login';
 import Menu from './components/Menu';
-import Search from './components/Search';
-import ShowRecords from './components/ShowRecords';
 import NewUserRegister from './components/NewUserRegister';
 import './assets/styles/styles.css';
 import SearchResult from './components/SearchResult';
 import ReadingAchievements from './components/ReadingAchievements';
-import MyPage from './components/MyPage';
-import UserPassChange from './components/UserPassChange';
-import UsernameChange from './components/UsernameChange';
-import UserEmailChange from './components/UserEmailChange';
 import SetReadingGoals from './components/SetReadingGoals';
 import AchievementsSettings from './components/AchievementsSettings';
 import NotFound from './components/NotFound';
+
 import OtpVerification from './components/OtpVerification';
+import ReadingAchievements from './components/ReadingAchievements';
+import ReadingHistory from './components/ReadingHistory';
+import ReadingRecord from './components/ReadingRecord';
+import Register from './components/Register';
+import Search from './components/Search';
+import SearchResult from './components/SearchResult';
+import SetReadingGoals from './components/SetReadingGoals';
+import ShowRecords from './components/ShowRecords';
+import UserSettings from './components/UserSettings';
 
 function App() {
     return (
@@ -25,17 +32,18 @@ function App() {
             <Routes>
                 <Route path="/" element={<Top />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
                 <Route path="/menu" element={<Menu />} />
+                <Route path="/reading-record" element={<ReadingRecord />} />
+                <Route path="/reading-history" element={<ReadingHistory />} />
+                <Route path="/reading-achievements" element={<ReadingAchievements />} />
                 <Route path="/searchBooks" element={<Search />} />
                 <Route path="/searchBooksResult" element={<SearchResult />}></Route>
                 <Route path="/showRecords" element={<ShowRecords />} />
                 <Route path="/userRegistration" element={<NewUserRegister />} />
                 <Route path="/otpVerification" element={<OtpVerification />} />
                 <Route path="/achievements" element={<ReadingAchievements />} />
-                <Route path="/myPage" element={<MyPage />} />
-                <Route path="/userPassChange" element={<UserPassChange />} />
-                <Route path="/usernameChange" element={<UsernameChange />} />
-                <Route path="/userEmailChange" element={<UserEmailChange />} />
+                <Route path="/myPage" element={<UserSettings />} />
                 <Route path="/setReadingGoals" element={<SetReadingGoals />}></Route>
                 <Route path="/achievementsSettings" element={<AchievementsSettings />}></Route>
                 <Route path="*" element={<NotFound />} ></Route>
