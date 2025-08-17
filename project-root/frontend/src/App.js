2aa99711ed94dd5be6e6aed98ce5cb468159aa94
Chosso <kurotak7255@gmail.com>
2025-08-17 11:52:03 +0900

    各種ファイルとURLを修正

--- project-root/frontend/src/App.js
+++ project-root/frontend/src/App.js
@@ -2,29 +2,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Top from './components/Top';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './assets/styles/styles.css';
-import AchievementsSettings from './components/AchievementsSettings';
 import Login from './components/Login';
 import Menu from './components/Menu';
 import NewUserRegister from './components/NewUserRegister';
 import './assets/styles/styles.css';
-import SearchResult from './components/SearchResult';
-import ReadingAchievements from './components/ReadingAchievements';
 import SetReadingGoals from './components/SetReadingGoals';
 import AchievementsSettings from './components/AchievementsSettings';
 import NotFound from './components/NotFound';
-
 import OtpVerification from './components/OtpVerification';
 import ReadingAchievements from './components/ReadingAchievements';
-import ReadingHistory from './components/ReadingHistory';
-import ReadingRecord from './components/ReadingRecord';
 import Register from './components/Register';
 import Search from './components/Search';
-import SearchResult from './components/SearchResult';
-import SetReadingGoals from './components/SetReadingGoals';
 import ShowRecords from './components/ShowRecords';
 import UserSettings from './components/UserSettings';
+import SearchResult from './components/SearchResult';
 
 function App() {
     return (
@@ -34,8 +26,6 @@ function App() {
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/menu" element={<Menu />} />
-                <Route path="/reading-record" element={<ReadingRecord />} />
-                <Route path="/reading-history" element={<ReadingHistory />} />
                 <Route path="/reading-achievements" element={<ReadingAchievements />} />
                 <Route path="/searchBooks" element={<Search />} />
                 <Route path="/searchBooksResult" element={<SearchResult />}></Route>
